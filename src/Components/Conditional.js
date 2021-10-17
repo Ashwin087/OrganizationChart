4d985eb81c2b379a47c6b713dbcfff2b702d1e2f
Ashwin <ashwin.singh.087@gmail.com>
2021-10-17 00:05:23 -0400

    clearing erros for deployment

--- src/Components/Conditional.js
+++ src/Components/Conditional.js
@@ -1,19 +1,20 @@
-import { useEffect } from "react"
+import { useEffect, useCallback } from "react"
 
 const Conditional = (props) => {
-    const colorChecker = () => {
-        if (props.path.Status === "Critical"){
+
+    const colorChecker = useCallback(() => {
+        if (props.path.Status === "Critical") {
             return "text red"
-        } else if (props.path.Status === "Warning"){
+        } else if (props.path.Status === "Warning") {
             return "text yellow"
         } else {
             console.log('this work?')
             return "text"
         }
-    }
+    }, [props.path.Status])
 
     useEffect(() => {
-        setInterval(colorChecker, 180000);
+        setInterval(colorChecker, 1800000);
     }, [colorChecker]);
 
     return(
